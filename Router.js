01bb93776fd14d79812d1266bb21a2cab60a3c1e
Sanyam Ratreja <sanyamratreja18@gmail.com>
2024-04-24 11:17:26 +0530

    added submit route

--- Router.js
+++ Router.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { fetchSubtopics } = require("./controllers/topicController");
-const { getTest, getTestChat } = require("./controllers/testController");
+const { addTest, getTestChat, getTest, saveSelectedOptionsToTest } = require("./controllers/testController");
 const { getDetails, getLearnChat } = require("./controllers/learnController");
 // const {geminiapi , getTest, getDetails } = require("./geminiapi")
 
 const app = express.Router();
 app.post("/add", fetchSubtopics );
-app.post("/test" , getTest);
+
+app.post("/test/new" , addTest);
+app.get("/test/:subtopicId", getTest);
 app.post("/test/chat", getTestChat);
+app.post("/test/save", saveSelectedOptionsToTest);
+
 app.post("/learn" , getDetails);
 app.post("/learn/chat" , getLearnChat);
 module.exports = app;
